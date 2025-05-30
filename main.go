7b065e9950fcd532352f4e39dbb22ba0548e6d52
Jordi Guillaumes i Pons <jguillaumes@gmail.com>
2025-05-30 21:41:06 +0200

    Initial commit (WIP)

---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"os"
+
+	"github.com/jguillaumes/xmit_reader/internal"
+)
+
+func main() {
+	// Command line arguments:
+	// --input <input_file>: The input XMIT file to process.
+	// --target <target_directory>: The directory where the output files will be saved.
+
+	inputFile := flag.String("input", "", "L'arxiu XMIT d'entrada a processar")
+	targetDir := flag.String("target", "", "Directori on es guardaran els arxius de sortida")
+
+	flag.Parse()
+
+	// Check if input file and target directory are provided
+	if *inputFile == "" || *targetDir == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	// Check if the targert directory exists
+	if _, err := os.Stat(*targetDir); os.IsNotExist(err) {
+		println("Target directory does not exist:", *targetDir)
+		os.Exit(1)
+	}
+
+	// Open the input file
+	inFile, err := os.Open(*inputFile)
+	if err != nil {
+		println("Error opening input file:", err.Error())
+		os.Exit(1)
+	}
+	defer inFile.Close()
+	// Process the input file and generate output files
+	var count int
+	reader := bufio.NewReader(inFile)
+	count, err = internal.ProcessXMITFile(reader, *targetDir)
+	if err != nil {
+		println("Error processing input file:", err.Error())
+		os.Exit(1)
+	}
+
+	if count == 0 {
+		println("No members extracted from the input file.")
+	} else {
+		println("Successfully processed", count, "members from the XMIT input file.")
+	}
+	os.Exit(0)
+}
